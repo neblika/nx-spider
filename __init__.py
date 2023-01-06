f587e4b24ba345a6fe7d434c290c70fe0233e575
Emerson-Britto <emersonbritto987@gmail.com>
2023-01-06 03:52:43 -0300

    [feat]: urls.json | modifier | extract_link and link domain

--- __init__.py
+++ __init__.py
@@ -1,32 +1,78 @@
 from bs4 import BeautifulSoup
+from modifier import Modifier
 from browser import Browser
 import json
+import re
+
 
 print("======================")
 print("=== Spider Project ===")
 print("======================")
 
 
-browser = Browser()
+
+class Spider:
+  def __init__(self):
+    super(Spider, self).__init__()
+    self.browser = Browser()
+    self.modifier = Modifier()
+    self.links = []
+    self._load_urls_list()
 
 
-with open("urls.json", "r") as rFile:
-  pages = json.load(rFile)
+  def _load_urls_list(self):
+    with open("urls.json", "r") as rFile:
+      self.links = json.load(rFile)
 
 
-for page in pages:
-  node = page.get("node")
-  browser.win.get(page["url"])
-  html = browser.win.page_source
+  def start(self):
+    for link in self.links:
+      print(f"searching: {link}")
+      rootLink, domain = self.get_domain(link)
+      nodeAttr = self.modifier.node_target(link)
+      self.browser.win.get(link)
+      html = self.browser.win.page_source
+      html_links = self.extract_links(html, rootLink)
 
-  if node:
+      print(html_links)
+
+      if nodeAttr:
+        node_target = self.get_node(html, search=nodeAttr)
+        print(node_target.get_text())
+      else:
+        soup = BeautifulSoup(html)
+        print(soup.get_text())
+
+    self.browser.quit()
+
+
+  def get_node(self, html, search):
     soup = BeautifulSoup(html)
-    nodeAttr = node.split("=")
+    nodeAttr = search.split("=")
     nodeMatches = soup.find_all(attrs={nodeAttr[0]: nodeAttr[1]})
     node_target = nodeMatches[0]
-    print(node_target.get_text())
-  else:
-    soup = BeautifulSoup(html)
-    print(soup.get_text())
+    return node_target
 
-  browser.quit()
+
+  def extract_links(self, html, rootLink):
+    soup = BeautifulSoup(html)
+    nodeMatches = soup.find_all(href=True)
+    links = []
+    for element in nodeMatches:
+      link = element["href"]
+      if not link.startswith("http"):
+        links.append(rootLink + link)
+      else:
+        links.append(link)
+    return links
+
+
+  def get_domain(self, link):
+    """ returns (root link, domain) """
+    matches = re.findall(r"(https?:\/\/(([a-zA-Z0-9]\.?-?_?)*))", link)
+    return matches[0][0], matches[0][1]
+
+
+if __name__ == '__main__':
+  spider = Spider()
+  spider.start()
---
+++ modifier.py
@@ -0,0 +1,25 @@
+import json
+import re
+
+
+
+class Modifier:
+	def __init__(self):
+		super(Modifier, self).__init__()
+		self.base_file_path = "modifies.json"
+		self._modifies = None
+		self._load_base_file()
+
+
+	def _load_base_file(self):
+		with open(self.base_file_path, "r") as rFile:
+		  self._modifies = json.load(rFile)
+
+
+	def node_target(self, link):
+		for modify in self._modifies:
+			if re.match(modify["regex"], link):
+				return modify["node"]
+
+
+# matches = re.findall(regex, str(resHtml))
